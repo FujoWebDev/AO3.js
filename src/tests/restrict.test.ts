8aeff3e6d38e58be26619d414bc25c92b1ce29f2
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-02-11 23:35:43 -0800

    Update restrict.test.ts

--- src/tests/restrict.test.ts
+++ src/tests/restrict.test.ts
@@ -1,6 +1,8 @@
+//NOTE: This test may fail if the owner of the example work changes it to unrestricted!
+
 import { checkLocks } from "..";
 
-describe("Checks status of a restricted work. NOTE: This test may fail if the owner of the example work changes it to unrestricted!", () => {
+describe("Checks status of a restricted work.", () => {
  test("Checks a known restricted work.", async () => {
   const work = await checkLocks( { workID: "15461226" } )
 
