550af26756b86a2a180c4bb4d288d7982b15ff29
Riazaia <riazaia.art@gmail.com>
2022-02-25 12:54:10 -0300

    Add tests for fetching required fields from a work

--- src/tests/works.test.ts
+++ src/tests/works.test.ts
@@ -1,64 +1,90 @@
-import { getWorkData } from "../index";
-import { getWorkUrl } from "../utils/works"
+import { getWorkData, getWork } from "../index";
+import { getWorkUrl } from "../utils/works";
 
 describe("Fetches data from url", () => {
-test("Fetches work id from url", async () => {
- const workData = await getWorkData({
-  url: "https://archiveofourown.org/works/36667228",
- });
+  test("Fetches work id from url", async () => {
+    const workData = await getWorkData({
+      url: "https://archiveofourown.org/works/36667228",
+    });
 
- expect(workData).toMatchObject({
-  workId: "36667228",
- })
-});
+    expect(workData).toMatchObject({
+      workId: "36667228",
+    });
+  });
 
-test("Fetches chapter id from url", async () => {
- const workData = await getWorkData({
-  url: "https://archiveofourown.org/works/398023/chapters/659774",
- });
+  test("Fetches chapter id from url", async () => {
+    const workData = await getWorkData({
+      url: "https://archiveofourown.org/works/398023/chapters/659774",
+    });
 
- expect(workData).toMatchObject({
-  workId: "398023",
-  chapterId: "659774",
- })
-});
+    expect(workData).toMatchObject({
+      workId: "398023",
+      chapterId: "659774",
+    });
+  });
 
-test("Fetches collection from url", async () => {
- const workData = await getWorkData({
-  url: "https://archiveofourown.org/collections/YJ_Prompts/works/30216801",
- });
+  test("Fetches collection from url", async () => {
+    const workData = await getWorkData({
+      url: "https://archiveofourown.org/collections/YJ_Prompts/works/30216801",
+    });
 
- expect(workData).toMatchObject({
-  workId: "30216801",
-  collectionName: "YJ_Prompts",
- })
-});
+    expect(workData).toMatchObject({
+      workId: "30216801",
+      collectionName: "YJ_Prompts",
+    });
+  });
 });
 
 describe("Gets url from data", () => {
- test("Gets url from workId", async () => {
-  const workUrl = await getWorkUrl(getWorkData({
-   url: "https://archiveofourown.org/works/36667228",
-  }))
-  
-  expect(workUrl).toBe("https://archiveofourown.org/works/36667228");
- });
- 
- test("Fetches chapter id from url", async () => {
-  const workUrl = await getWorkUrl({
-   workId: "398023",
-   chapterId: "659774",
-  })
- 
-  expect(workUrl).toBe("https://archiveofourown.org/works/398023/chapters/659774");
- });
- 
- test("Fetches collection from url", async () => {
-  const workUrl = await getWorkUrl({
-   workId: "30216801",
-   collectionName: "YJ_Prompts",
-  })
+  test("Gets url from workId", async () => {
+    const workUrl = await getWorkUrl(
+      getWorkData({
+        url: "https://archiveofourown.org/works/36667228",
+      })
+    );
 
-  expect(workUrl).toBe("https://archiveofourown.org/collections/YJ_Prompts/works/30216801")
- });
- });
+    expect(workUrl).toBe("https://archiveofourown.org/works/36667228");
+  });
+
+  test("Fetches chapter id from url", async () => {
+    const workUrl = await getWorkUrl({
+      workId: "398023",
+      chapterId: "659774",
+    });
+
+    expect(workUrl).toBe(
+      "https://archiveofourown.org/works/398023/chapters/659774"
+    );
+  });
+
+  test("Fetches collection from url", async () => {
+    const workUrl = await getWorkUrl({
+      workId: "30216801",
+      collectionName: "YJ_Prompts",
+    });
+
+    expect(workUrl).toBe(
+      "https://archiveofourown.org/collections/YJ_Prompts/works/30216801"
+    );
+  });
+});
+
+describe("Fetches work data", () => {
+  test("Fetches required work data", async () => {
+    const work = await getWork({
+      workId: "37040272",
+    });
+
+    expect(work).toMatchObject({
+      author: "Riazaia",
+      title: "Solo lo Obligatorio",
+      rating: ["Not Rated"],
+      warning: ["No Archive Warnings Apply"],
+      fandom: ["Testing"],
+      language: "Español",
+      published: "1969-06-09",
+      words: "124",
+      chapters: "1/1",
+    });
+  });
+});
