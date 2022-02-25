a768ed2c107d374b834bc14e4062314bc0f06ccb
Riazaia <riazaia.art@gmail.com>
2022-02-25 12:53:28 -0300

    Add methods to fetch basic info and required fields from a work

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,34 @@ import {
   isCommon,
 } from "./utils/tags";
 import { getFeedPage, getTagNameFromFeed } from "./utils/feed";
-import { 
-  getTagId, 
+import {
+  getTagId,
   getWorksPage,
   WorkData,
+  Work,
+  getWorkPage,
+  getWorkTitle,
+  getWorkAuthor,
+  getWorkRating,
+  getWorkWarning,
+  getWorkFandom,
+  getWorkLanguage,
+  getWorkPublished,
+  getWorkWords,
+  getWorkChapters,
 } from "./utils/works";
-import { 
- User,
- getProfile, 
- getProfileLink, 
- getProfileName, 
- getProfilePseuds,
- getProfileJoined,
- getProfileID,
- getProfileBio,
- getProfileEmail,
- getProfileLocation,
- getProfileBday
+import {
+  User,
+  getProfile,
+  getProfileLink,
+  getProfileName,
+  getProfilePseuds,
+  getProfileJoined,
+  getProfileID,
+  getProfileBio,
+  getProfileEmail,
+  getProfileLocation,
+  getProfileBday,
 } from "./utils/user";
 
 import axios from "axios";
@@ -56,35 +67,49 @@ export const getTagNameById = async ({ tagId }: { tagId: string }) => {
 };
 
 export const getUser = async ({
- userName,
+  userName,
 }: {
- userName: string;
+  userName: string;
 }): Promise<User> => {
+  const profilePage = await getProfile(userName);
 
- const profilePage = await getProfile(userName)
-
- return {
-   name: getProfileName(profilePage),
-   pseuds: getProfilePseuds(profilePage),
-   id: getProfileID(profilePage),
-   joined: getProfileJoined(profilePage),
-   email: getProfileEmail(profilePage),
-   location: getProfileLocation(profilePage),
-   birthday: getProfileBday(profilePage),
-   url: getProfileLink(userName),
-   bioHtml: getProfileBio(profilePage),
- };
+  return {
+    name: getProfileName(profilePage),
+    pseuds: getProfilePseuds(profilePage),
+    id: getProfileID(profilePage),
+    joined: getProfileJoined(profilePage),
+    email: getProfileEmail(profilePage),
+    location: getProfileLocation(profilePage),
+    birthday: getProfileBday(profilePage),
+    url: getProfileLink(userName),
+    bioHtml: getProfileBio(profilePage),
+  };
 };
 
-export const getWorkData = ({
-  url,
- }: {
-  url: string;
- }): WorkData => {
-   
+export const getWorkData = ({ url }: { url: string }): WorkData => {
   return {
     workId: url.match(/works\/(\d+)/)[1],
     chapterId: url.match(/chapters\/(\d+)/)?.[1],
     collectionName: url.match(/collections\/(\w+)/)?.[1],
   };
-}
+};
+
+export const getWork = async ({
+  workId,
+}: {
+  workId: string;
+}): Promise<Work> => {
+  const workPage = await getWorkPage(workId);
+
+  return {
+    author: getWorkAuthor(workPage),
+    title: getWorkTitle(workPage),
+    rating: getWorkRating(workPage),
+    warning: getWorkWarning(workPage),
+    fandom: getWorkFandom(workPage),
+    language: getWorkLanguage(workPage),
+    published: getWorkPublished(workPage),
+    words: getWorkWords(workPage),
+    chapters: getWorkChapters(workPage),
+  };
+};
--- src/utils/works.ts
+++ src/utils/works.ts
@@ -2,6 +2,7 @@ import cheerio, { CheerioAPI } from "cheerio";
 
 import axios from "axios";
 import { getTagUrl } from "./tags";
+import { emitWarning } from "process";
 
 // We create separate interfaces for each page type to make sure that the
 // correct type of page is passed to each method that extracts data.
@@ -53,5 +54,83 @@ export const getWorkUrl = ({
     workUrl += `/chapters/${chapterId}`;
   }
 
-  return workUrl
+  return workUrl;
 };
+
+export const getWorkPage = async (workId: string) => {
+  return cheerio.load(
+    (
+      await axios.get<string>(`https://archiveofourown.org/works/${workId}`, {
+        // We set a cookie to bypass the Terms of Service agreement modal that appears when viewing works as a guest, which prevented some selectors from working. Appending ?view_adult=true to URLs doesn't work for chaptered works since that part gets cleared when those are automatically redirected.
+        headers: {
+          Cookie: "view_adult=true;",
+        },
+      })
+    ).data
+  ) as WorkPage;
+};
+
+export interface Work {
+  author: string;
+  title: string;
+  rating: string[];
+  warning: string[];
+  fandom: string[];
+  language: string;
+  published: string;
+  words: string;
+  chapters: string;
+}
+
+// Start of methods that fetch required fields
+export const getWorkAuthor = ($workPage: WorkPage) => {
+  const author = [];
+  $workPage("h3.byline").each(function (i, element) {
+    author[i] = $workPage(element).text().trim();
+  });
+  return author.join();
+};
+
+export const getWorkTitle = ($workPage: WorkPage) => {
+  return $workPage("h2.title").text().trim();
+};
+
+export const getWorkRating = ($workPage: WorkPage) => {
+  const rating = [];
+  $workPage("dd.rating ul.commas li").each(function (i, element) {
+    rating[i] = $workPage(element).text().trim();
+  });
+  return rating;
+};
+
+export const getWorkWarning = ($workPage: WorkPage) => {
+  const warning = [];
+
+  $workPage("dd.warning ul.commas li").each(function (i, element) {
+    warning[i] = $workPage(element).text().trim();
+  });
+  return warning;
+};
+
+export const getWorkFandom = ($workPage: WorkPage) => {
+  const fandom = [];
+  $workPage("dd.fandom ul.commas li").each(function (i, element) {
+    fandom[i] = $workPage(element).text().trim();
+  });
+  return fandom;
+};
+
+export const getWorkLanguage = ($workPage: WorkPage) => {
+  return $workPage("dd.language").text().trim();
+};
+
+export const getWorkPublished = ($workPage: WorkPage) => {
+  return $workPage("dd.published").text().trim();
+};
+export const getWorkWords = ($workPage: WorkPage) => {
+  return $workPage("dd.words").text().trim();
+};
+export const getWorkChapters = ($workPage: WorkPage) => {
+  return $workPage("dd.chapters").text().trim();
+};
+// End of methods that fetch required fields
