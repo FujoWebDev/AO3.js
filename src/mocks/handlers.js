0d0f37649a685737e481131a60a908eee726ec90
Jake Hamilton <jake.hamilton@hey.com>
2022-04-12 01:50:08 -0700

    chore(tests): add msw for mocking api http requests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,7 @@
+module.exports = [
+  require("./handlers/users/profile"),
+  require("./handlers/tags/feed"),
+  require("./handlers/tags/works"),
+  require("./handlers/tags/name"),
+  require("./handlers/all"),
+];
