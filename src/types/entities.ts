a773f371c734a86873d7cd9f801c2d07187402e8
Essential Randomness <essential.randomn3ss@gmail.com>
2022-07-22 16:45:20 -0700

    add type for summary of work

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -30,3 +30,68 @@ export interface User {
   birthday: string | null;
   bioHtml: string | null;
 }
+
+export enum WorkRatings {
+  GENERAL_AUDIENCES = "General Audiences",
+  TEEN_AND_UP_AUDIENCES = "Teen And Up Audiences",
+  MATURE = "Mature",
+  EXPLICIT = "Explicit",
+}
+
+export enum WorkCategory {
+  FF = "F/F",
+  FM = "F/M",
+  GEN = "Gen",
+  MM = "M/M",
+  MULTI = "Multi",
+  OTHER = "Other",
+}
+
+export enum WorkWarningStatus {
+  CHOOSE_NOT_TO_WARN = "Author chose not to warn",
+  EXTERNAL = "External work",
+  HAS_WARNING = "Work has one or more warning",
+}
+
+export enum WorkWarnings {
+  GRAPHIC_VIOLENCE = "Graphic depictions of violence",
+  MAJOR_CHARACTER_DEATH = "Major character death",
+  NONCON = "Rape/non-con",
+  UNDERAGE = "underage sex",
+}
+
+export interface WorkSummary {
+  title: string;
+  category: WorkCategory | null;
+  publishedAt: number;
+  // TODO: should this be in HTML?
+  summary: string;
+  rating: WorkRatings | null;
+  // TODO: is this the same as rating?
+  adult: boolean;
+  // TODO: can this be null?
+  fandoms: string[];
+  warningStatus: WorkWarningStatus;
+  tags: {
+    warnings: WorkWarnings[];
+    relationships: string[];
+    additional: string[];
+  };
+  // TODO: how do we represent orphaned works? Can author be null?
+  author: {
+    id: number;
+    name: string;
+  };
+  language: string;
+  words: number;
+  chapters: {
+    current: number;
+    total: number | null;
+    complete: boolean;
+  };
+  stats: {
+    comments: number;
+    kudos: number;
+    hits: number;
+  };
+}
