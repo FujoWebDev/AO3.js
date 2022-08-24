730f7de0788eb5025209d5d5d813a856d1de8afa
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-24 16:24:01 -0700

    use windows-compatible file names in tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,19 @@
+const ES_MODULES = [
+  "filenamify",
+  "trim-repeated",
+  "escape-string-regexp",
+  "filename-reserved-regex",
+  "strip-outer",
+].join("|");
+
 export default {
   testEnvironment: "node",
   transform: {
-    "\\.(ts|tsx)$": "ts-jest",
+    "\\.m?(ts|tsx)$": "ts-jest",
+    [`/node_modules/(${ES_MODULES})`]: "jest-esm-transformer",
   },
+  transformIgnorePatterns: [`/node_modules/(?!${ES_MODULES})`],
   testPathIgnorePatterns: ["<rootDir>/dist/"],
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
   setupFilesAfterEnv: ["./jest.setup.ts"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
 };
