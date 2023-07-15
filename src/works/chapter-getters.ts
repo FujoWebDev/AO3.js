6bf89d316a2abe786e5b76039f571c2f58870316
Essential Randomness <essential.randomn3ss@gmail.com>
2023-07-14 17:57:52 -0700

    add methods to fetch chapters

---
+++ src/works/chapter-getters.ts
@@ -0,0 +1,56 @@
+import { Author, Chapter } from "types/entities";
+import { getWorkDetailsFromUrl, getWorkUrl } from "src/urls";
+
+import { ChapterIndexPage } from "src/page-loaders";
+
+const TITLE_SEPARATOR = ". ";
+export const getChaptersList = ($chapterIndexPage: ChapterIndexPage) => {
+  const chapters: Chapter[] = [];
+  $chapterIndexPage("ol.index > li").each((index, li) => {
+    const link = $chapterIndexPage(li).find("a")[0];
+    const chapterText = $chapterIndexPage(link).text();
+    const { workId, chapterId } = getWorkDetailsFromUrl({
+      url: link.attribs.href,
+    });
+    const title = chapterText.substring(
+      chapterText.indexOf(TITLE_SEPARATOR) + TITLE_SEPARATOR.length
+    );
+    const dateNode = $chapterIndexPage(
+      $chapterIndexPage(li).find(".datetime")[0]
+    );
+
+    chapters.push({
+      id: chapterId!,
+      workId,
+      index: index + 1,
+      title,
+      // Remove parenthesis from the date
+      publishedAt: dateNode.text().replace(/[\(\)]/g, ""),
+      // We rebuild the url so it gets the full path
+      url: getWorkUrl({ workId, chapterId }),
+    });
+  });
+  return chapters;
+};
+
+export const getWorkTitle = ($chapterIndexPage: ChapterIndexPage) => {
+  return $chapterIndexPage(".works-navigate h2 a[href^='/works/']").text();
+};
+
+export const getWorkAuthors = ($chapterIndexPage: ChapterIndexPage) => {
+  const authors: Author[] = [];
+  const authorNode = $chapterIndexPage(".works-navigate h2 a[rel='author']");
+  if (authorNode.text().trim() === "Anonymous") {
+    return "Anonymous";
+  }
+
+  if (authorNode.length !== 0) {
+    authorNode.each((i, element) => {
+      const url = element.attribs.href;
+      const [, username, pseud] = url.match(/users\/(.+)\/pseuds\/(.+)/)!;
+
+      authors.push({ username: username, pseud: decodeURI(pseud) });
+    });
+  }
+  return authors;
+};
