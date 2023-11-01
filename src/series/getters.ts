28d840da6c5ad75347b2ac94e0c17a19191c2171
H. Kamran <hkamran@hkamran.com>
2023-10-31 22:40:45 -0700

    Add some tests

--- src/series/getters.ts
+++ src/series/getters.ts
@@ -65,7 +65,6 @@ export const getSeriesDescription = (
 export const getSeriesNotes = ($seriesPage: SeriesPage): string | null => {
   const notes = $seriesPage("dl.series dd:nth-of-type(5)");
   if (notes.prevAll().first().text().trim() === "Notes:") {
-    console.log("Yep");
     return notes.html().trim();
   } else {
     return null;
