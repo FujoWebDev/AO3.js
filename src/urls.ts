c1ffadc56092e305bc4af27f879ea8280386ec86
Rabbit Pie <piradiabbit@gmail.com>
2024-02-23 18:23:35 +1030

    deleted extraneous examples and comments

--- src/urls.ts
+++ src/urls.ts
@@ -58,24 +58,6 @@ export const getWorkDetailsFromUrl = ({
   };
 };
 
-//defining the form of the arguments and the output
-export const TODODelete = ({url,}: {url: string;}): {
-  workId: string;
-  chapterId?: string;
-  collectionName?: string;
-} => {
-  const workUrlMatch = url.match(/works\/(\d+)/);
-  if (!workUrlMatch) {
-    throw new Error("Invalid work URL");
-  }
-
-  return {
-    workId: workUrlMatch[1],
-    chapterId: url.match(/chapters\/(\d+)/)?.[1],
-    collectionName: url.match(/collections\/(\w+)/)?.[1],
-  };
-};
-
 
 export const getPromptDetailsFromUrl = ({
   // defining the input structure, eg.
@@ -88,7 +70,7 @@ export const getPromptDetailsFromUrl = ({
   //collection name is the url name, not the Display Title
   promptId: string;
   collectionName: string
-} => {//TODO will fail?
+} => {
   const promptUrlMatch = url.match(/prompts\/(\d+)/);
   if (!promptUrlMatch) {
     throw new Error("Invalid prompt URL");
