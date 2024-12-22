4b8b8965c9e4207afd2781824848e67a79946acc
Rhys Kim <hello@rhyses-pieces.dev>
2024-12-22 13:47:32 -0500

    explain the nested subtag situation

--- src/tags/page-getters.ts
+++ src/tags/page-getters.ts
@@ -59,6 +59,7 @@ export const getSubTags = ($tagPage: TagPage) => {
     let tags: string[] = [];
     if ($tagPage($tagPage(element).has("ul"))) {
       $tagPage("ul.tags", element).children("li").each((_, child) => {
+        // If a given subtag has another list of subtags, this flattens the list into a single array
         if ($tagPage(child).has("ul")) {
           tags.push($tagPage(child).children().first().text());
         } else {
