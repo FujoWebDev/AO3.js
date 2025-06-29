bebec3eb4e9ad6fb61ef30740b07f375b6ca513d
ginger <26461046+gingerchew@users.noreply.github.com>
2025-06-28 20:58:16 -0400

    Removes author file

--- src/author/index.ts
+++
@@ -1,9 +0,0 @@
-// I'll move this to the right spot, just a temporary thing to get a draft PR going
-interface IgetAuthorWorks {
-  username: string;
-  page: number;
-}
-
-export const getAuthorWorks = ({ username, page = 0}) => {
-
-}
