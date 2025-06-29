428d4703c9ae6307f3de27524d4b998f53d8b4e2
ginger <26461046+gingerchew@users.noreply.github.com>
2025-06-28 20:28:06 -0400

    Adds initial getAuthorWorks for draft PR start

---
+++ src/author/index.ts
@@ -0,0 +1,9 @@
+// I'll move this to the right spot, just a temporary thing to get a draft PR going
+interface IgetAuthorWorks {
+  username: string;
+  page: number;
+}
+
+export const getAuthorWorks = ({ username, page = 0}) => {
+
+}
