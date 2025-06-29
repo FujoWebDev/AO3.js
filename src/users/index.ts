615db7a6782d99c249d0bdc1f604f454127ec3b6
ginger <26461046+gingerchew@users.noreply.github.com>
2025-06-28 21:22:29 -0400

    Adds initial implementation
    Data structure is still a little goofy and doesn't match what was discussed in the discord

--- src/users/index.ts
+++ src/users/index.ts
@@ -15,9 +15,9 @@ import {
   getUserProfileWorks,
 } from "./getters";
 
-import { User, WorkSummary } from "types/entities";
+import { User, WorkPreview, WorkSummary } from "types/entities";
 import { getUserProfileUrl } from "../urls";
-import { loadUserProfilePage, loadUserWorksList, loadWorkPage } from "../page-loaders";
+import { loadUserProfilePage, loadUserWorksList, loadWorkPage, UserWorksPage } from "../page-loaders";
 
 export const getUser = async ({
   username,
@@ -47,12 +47,47 @@ export const getUser = async ({
   };
 };
 
-export const getUserWorks = async ({ username }: {username: string}): Promise<WorkSummary[]> => {
+const parseUserWorksIntoObject = ($userWorks: UserWorksPage) => {
+  /**
+   * It's just easier for me to reason this way,
+   * I can move this into a more correct file later
+   */
+  const itemSelector = '.index.work.group > li';
+  const selectors = {
+    kudos: '.kudos + .kudos a',
+    comments: '.comments + .comments a',
+    chapters: '.chapters + .chapters a',
+    words: '.words + .words',
+    hits: '.hits + .hits',
+    bookmarks: '.bookmarks + .bookmarks a',
+    title: '.heading a:first-child',
+    fandom: '.fandoms a',
+    category: '.category .text',
+    rating: '.rating .text',
+    warnings: '.warnings .tag',
+    complete: '.iswip .text'
+  }
+  const numberKeys = ['kudos','comments','chapters','words','hits','bookmarks']
+  const works = [];
+  // unfortunately $userWorks(selector).map doesn't return an Array, it returns a Cheerio
+  $userWorks(itemSelector).each((_i, el) => {
+    const data = {};
+    const $item = $userWorks(el);
+    for (const [key, selector] of Object.entries(selectors)) {
+      data[key] = numberKeys.includes(key) ? parseInt($item.find(selector).text(), 10) : $item.find(selector).text();
+    }
+    works.push(data);
+  })
+
+  return works
+}
+
+export const getUserWorks = async ({ username }: { username: string }): Promise<WorkSummary[]> => {
   const worksPage = await loadUserWorksList({ username });
   // parse current works page
   // check for next page
   // if next page
-    // loop it
+  // loop it
   // else return data
   return [];
 }
