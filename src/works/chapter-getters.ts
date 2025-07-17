439b3cf4345372be32bb924e7514133314a1c094
ginger <26461046+gingerchew@users.noreply.github.com>
2025-07-17 00:35:11 -0400

    Add shortlink support (#114)
    * Adds getAsShortUrl util function
    
    * Adds shortUrl property to interfaces with url property
    
    * Updates return for related shortUrl objects
    
    * Adds export for getAsShortUrl to the main index file

--- src/works/chapter-getters.ts
+++ src/works/chapter-getters.ts
@@ -1,5 +1,5 @@
 import { Author, Chapter } from "types/entities";
-import { getWorkDetailsFromUrl, getWorkUrl } from "src/urls";
+import { getAsShortUrl, getWorkDetailsFromUrl, getWorkUrl } from "src/urls";
 
 import { ChapterIndexPage } from "src/page-loaders";
 
@@ -18,7 +18,8 @@ export const getChaptersList = ($chapterIndexPage: ChapterIndexPage) => {
     const dateNode = $chapterIndexPage(
       $chapterIndexPage(li).find(".datetime")[0]
     );
-
+    const url = getWorkUrl({ workId, chapterId });
+    const shortUrl = getAsShortUrl({ url });
     chapters.push({
       id: chapterId!,
       workId,
@@ -27,7 +28,8 @@ export const getChaptersList = ($chapterIndexPage: ChapterIndexPage) => {
       // Remove parenthesis from the date
       publishedAt: dateNode.text().replace(/[\(\)]/g, ""),
       // We rebuild the url so it gets the full path
-      url: getWorkUrl({ workId, chapterId }),
+      url,
+      shortUrl
     });
   });
   return chapters;
