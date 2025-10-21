84f243d5ccba974a7c6621f567bddb86b20f0fbc
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-10-21 16:37:16 -0700

    move library bundler to tsdown

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    lib: {
-      name: "AO3.js",
-      entry: {
-        index: resolve(__dirname, "src/index.ts"),
-        urls: resolve(__dirname, "src/urls.ts"),
-      },
-      formats: ["es"],
-    },
-  },
   plugins: [dts({ rollupTypes: true }), tsconfigPaths()],
 });
